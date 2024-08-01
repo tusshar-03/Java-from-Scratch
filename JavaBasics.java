b11bc1e88b827da26e0d8d51d80a2c362b1dc42d
Tusshar Garg <109620149+tusshar-03@users.noreply.github.com>
2024-08-01 18:40:58 +0530

    Add files via upload

---
+++ JavaBasics.java
@@ -0,0 +1,9 @@
+public class JavaBasics { 
+    public static void main(String args[]) //for Execution of java code
+    { 
+        System.out.println("Hello Tusshar\n ");//print output
+        System.out.println("Hello Tusshar");
+    }
+}
+//boilerplate code  
+ 
