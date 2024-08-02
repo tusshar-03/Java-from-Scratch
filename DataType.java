14c9210042557dd6bdbd0afb115c61c4b44a9778
Tusshar Garg <109620149+tusshar-03@users.noreply.github.com>
2024-08-02 16:51:33 +0530

    Add files via upload

---
+++ DataType.java
@@ -0,0 +1,22 @@
+public class DataType {
+    public static void main(String[] args) {
+        byte b = 8;
+        System.out.println(b);
+
+        char ch= 'a'; //only single Character can be stored
+        System.out.println(ch);
+
+        boolean var = false; //used for condition values
+        System.out.println(var);
+
+        float p = 105; //Decimal values
+        System.out.println(p);
+
+        //long 
+        short n = 22;
+        System.out.println(n);
+        //double
+
+
+    }
+}
---
+++ Sum.java
@@ -0,0 +1,9 @@
+public class Sum {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 50;
+        int sum = a + b;
+        System.out.println(sum);
+        
+    }
+}
