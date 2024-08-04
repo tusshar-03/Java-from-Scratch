0d58e8ee4986bf5619de9fe1f17e73553014f482
Tusshar Garg <109620149+tusshar-03@users.noreply.github.com>
2024-08-04 10:38:22 +0530

    Add files via upload

---
+++ InputSum.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class InputSum { 
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int a= sc.nextInt();
+        int b = sc.nextInt();
+
+        int Sum = a + b;
+        
+        System.out.print("Sum = ");
+        System.out.println(Sum);
+    }
+}
---
+++ Product.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Product {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int Product = a*b;
+
+        System.out.println(Product);
+    }
+}
